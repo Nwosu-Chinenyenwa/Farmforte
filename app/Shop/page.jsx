f077569398fd87f410f0833af3ab8ddcdb4272a9
Nwosu chinenyenwa <nwosuchinenyenwa4@gmail.com>
2025-11-01 13:40:02 +0100

    still trying

--- app/Shop/page.jsx
+++ app/Shop/page.jsx
@@ -1,11 +1,11 @@
 "use client";
 
-import React, { Suspense } from "react";
+import React from "react";
 import Shop from "../Components/Shop";
 
 export default function ShopPage() {
   return (
-    <Suspense fallback={
+ /*    <Suspense fallback={
       <div className="flex flex-col gap-4 items-center justify-center min-h-screen">
         <div className="w-20 h-20 border-4 border-transparent animate-spin border-t-blue-400 rounded-full flex items-center justify-center">
           <div className="w-16 h-16 border-4 border-transparent animate-spin border-t-[#209e2e] rounded-full"></div>
@@ -14,6 +14,7 @@ export default function ShopPage() {
       </div>
     }>
       <Shop/>
-    </Suspense>
+    </Suspense> */
+    <></>
   );
 }
