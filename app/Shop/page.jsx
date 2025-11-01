0aafe419617c2c7246cbd2964721aacb5d33cb54
Nwosu chinenyenwa <nwosuchinenyenwa4@gmail.com>
2025-11-01 13:47:29 +0100

    try again

--- app/Shop/page.jsx
+++ app/Shop/page.jsx
@@ -1,20 +1,9 @@
 "use client";
 
 import React from "react";
-import Shop from "../Components/Shop";
 
 export default function ShopPage() {
   return (
- /*    <Suspense fallback={
-      <div className="flex flex-col gap-4 items-center justify-center min-h-screen">
-        <div className="w-20 h-20 border-4 border-transparent animate-spin border-t-blue-400 rounded-full flex items-center justify-center">
-          <div className="w-16 h-16 border-4 border-transparent animate-spin border-t-[#209e2e] rounded-full"></div>
-        </div>
-        <p className="text-[#209e2e] font-semibold">Loading shop...</p>
-      </div>
-    }>
-      <Shop/>
-    </Suspense> */
     <></>
   );
 }
